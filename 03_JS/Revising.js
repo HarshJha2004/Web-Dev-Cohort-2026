3d7f40794c1c991f753b49a2bbc0bd99d85634cf
Harsh Jha <Harshwork5546@gmail.com>
2026-03-17 14:42:10 +0530

    Till then L-2 Practice Added

--- 03_JS/Revising.js
+++ 03_JS/Revising.js
@@ -22,14 +22,44 @@
     // In arrow function when only one statement have to execute, that time it's not need of the {}, and also return keyword.
     
     const addTwoNumbers = (num1, num2) => num1 + num2;
-    console.log(addTwoNumbers(5, 3));
+    // console.log(addTwoNumbers(5, 3));
+
 // // just for DRY Run:
     function cartoon1(){
     function cartoonInsideCartoon(){
         return 'Naruto';
     }
     return cartoonInsideCartoon;
-}
+    }
 
+// // Array
+    const fruits = ["mango", "Banana", "Apple", "Guava", false, 32];  //Array have index-based nature.
+
+    // console.log(fruits[1]);
+    // fruits.push("papaya") // To add elements end of the array.
+    // console.log(fruits.length); // To check the array length.
+
+    // console.log(fruits.includes("papaya")); // To check element present in array or not.
+
+    // console.log(fruits.slice(3, 5));
+
+    // console.log(fruits[3]);
+
+// // Objects
+    const personDetails = {
+        name : "Harsh Jha",
+        age : 21,
+        Sex : "Male",
+        BOD : "28/12/2004"
+    }
+
+    // console.log(typeof console.log);
+
+    let choice = false;
+    let strConvert = String(choice);
+
+    console.log(strConvert);
+    
+    
 
 
